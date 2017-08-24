d921aa4e3498dc6558025e0be7b0731b510f4ff3
Kai Laborenz <k.laborenz@familie-redlich.de>
2017-08-24 12:34:45 +0200

    Added different reaction scheme: depending on number of switch actions speed is different and led (on pin 5) is lit

--- Useless_box.ino
+++ Useless_box.ino
@@ -1,4 +1,4 @@
-// Useless Box v1.0
+// Useless Box v2.0
 //
 // Kai Laborenz 2017
 //
@@ -10,6 +10,7 @@
 
 // const and variables definitions
 const int  buttonPin = 2;
+const int  ledPin = 5;
 int buttonState = 0;
 
 Servo myservo;        // create servo object to control a servo
@@ -22,6 +23,8 @@ int counterSwitch = 0;
 void setup()
 {
   pinMode(buttonPin, INPUT);
+  pinMode(ledPin, OUTPUT);
+  
   myservo.attach(9);    // attaches servo on pin 9 to the servo object
 
   Serial.begin(9600); // initialize serial communication at 9600 bits per second:
@@ -35,7 +38,37 @@ void loop()
 
 
 switch (counter) {
-    case 4:
+    case 1:
+       if (buttonState == HIGH) {  // not switched
+                                  
+         if (counterSwitch == 0) {                      
+           counter = counter + 1;    // advance counter
+           counterSwitch = 1;    
+           Serial.println(counter);
+           }
+      
+         for(pos = myservo.read(); pos >=20; pos -= 1) {                                     
+           myservo.write(pos);     
+           }
+
+           digitalWrite(ledPin, LOW);
+        }
+        else {                    // switched
+        
+          // if (counter % 2 == 0) { timeDelay = 50; } // every second round goes slower
+
+          digitalWrite(ledPin, HIGH);
+          
+          for(pos = myservo.read(); pos <=90; pos += 1) {                    
+            myservo.write(pos);                 
+            delay(20);   
+            }
+            counterSwitch = 0;
+          }
+     
+      break;
+
+    case 3:
        if (buttonState == HIGH) {  // not switched
                                   
          if (counterSwitch == 0) {                      
@@ -50,18 +83,20 @@ switch (counter) {
         }
         else {                    // switched
         
-          // if (counter % 2 == 0) { timeDelay = 50; } // every second round goes slower
+          delay(1000); 
           
           for(pos = myservo.read(); pos <=90; pos += 1) {                    
-            myservo.write(pos);                 
+            myservo.write(pos);    
+            digitalWrite(ledPin, HIGH);             
             delay(50);   
+            digitalWrite(ledPin, LOW);
             }
             counterSwitch = 0;
           }
      
       break;
       
-    case 6:
+    case 5:
        if (buttonState == HIGH) {  // not switched
                                   
          if (counterSwitch == 0) {                      
@@ -81,12 +116,12 @@ switch (counter) {
             delay(50);   
             }
             delay(1000); 
-            myservo.write(90);  
+            myservo.write(89);  
             counterSwitch = 0;
           }
       break;
 
-    case 1:
+    case 7:
        if (buttonState == HIGH) {  // not switched
                                   
          if (counterSwitch == 0) {                      
@@ -101,27 +136,50 @@ switch (counter) {
         }
         else {                    // switched
 
-          delay(1000); 
+          delay(2000); 
           
           for(pos = myservo.read(); pos <=88; pos += 1) {                    
             myservo.write(pos);                 
-            delay(2);   
+            delay(10);   
             }
             delay(500); 
 
             for(pos = myservo.read(); pos >=50; pos -= 1) {                                     
               myservo.write(pos);     
               }
-            delay(1500); 
+            delay(500); 
             
            for(pos = myservo.read(); pos <=85; pos += 1) {                    
             myservo.write(pos);                 
-            delay(50);   
+            delay(10);   
             }
-            delay(1500); 
+            delay(500); 
+            
+            for(pos = myservo.read(); pos >=50; pos -= 1) {                                     
+              myservo.write(pos);     
+              }
+            delay(500); 
+
+            for(pos = myservo.read(); pos <=85; pos += 1) {                    
+            myservo.write(pos);                 
+            delay(10);   
+            }
+            delay(500); 
+
+            for(pos = myservo.read(); pos >=20; pos -= 1) {                                     
+           myservo.write(pos);   
+           delay(100);  
+           }
+
+            
             counterSwitch = 0;
+
           }
       break;
+
+      case 9:
+          counter = 0;
+      break; 
       
     default:
        if (buttonState == HIGH) {  // not switched
@@ -151,5 +209,6 @@ switch (counter) {
         }
 
 }                                 // end main loop
+
 
 
